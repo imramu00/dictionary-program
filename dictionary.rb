909953eb370034cb5e22db23896188c46bd36115
imramu00 <imramu00@gmail.com>
2021-05-26 22:58:35 +0530

    Added Documentation

--- dictionary.rb
+++ dictionary.rb
@@ -2,13 +2,20 @@
 require 'net/http'
 require 'json'
 
+# Class dictionary holds methods to find meaning for a word and suggestion of words
 class Dictionary
   attr_accessor :word
 
+  # initializer of the class taking a string as argument
   def initialize(word)
     @word=word
   end
 
+  # response method
+  ## this method takes a word as it's argument and gets the meaning of the given word by REST API
+  ## The response will be in JSON format
+  ## The JSON file can be parsed using JSON gem
+  ## The parsed JSON will give a Hash
   def response(word)
     url = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/'+word
     uri = URI(url)
@@ -16,6 +23,8 @@ class Dictionary
     json_data =  JSON.parse(response)
   end
 
+  # word_checker
+  ## this method takes an array of strings as the argument and returns the first valid english string in the array.
   def word_checker(word_possibility)
     valid_words=[]
     word_possibility.each{ |word|
@@ -29,6 +38,8 @@ class Dictionary
     valid_words
   end
 
+  # word_predictor
+  ## this method takes an array of strings as argument and returns an array with all the possible english words starting with each string of the input array.
   def word_predictor(word_array)
     similar_words=[]
       word_array.each { |word|
